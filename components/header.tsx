ea6a7ad3881b5636881f85d70a99f7ae34845cbf
eliterajkumar <dineshchan952129@gmail.com>
2025-02-09 21:46:22 +0530

    contact page updated

--- components/header.tsx
+++ components/header.tsx
@@ -18,13 +18,8 @@ export function Header() {
           <Link href="/contact" className="text-gray-600 hover:text-primary">
             Contact
           </Link>
-<<<<<<< HEAD
-          <Link href="#contact" className="text-gray-600 hover:text-primary">
-           The AI Codex 
-=======
-          <Link href="#The AI Codex" className="text-gray-600 hover:text-primary">
+          <Link href="#ai-codex" className="text-gray-600 hover:text-primary">
             The AI Codex
->>>>>>> 0bb4d0f (contact page updated)
           </Link>
         </nav>
         <div className="flex items-center space-x-4">
